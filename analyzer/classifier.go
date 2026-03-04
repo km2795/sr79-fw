b67726bd8ca87ce4b0011b12d7d52e32ca7ab080
km2795 <kunalkm2795@gmail.com>
2026-03-05 00:24:02 +0530

    analyzer: define Packet struct and Classifier interface

--- analyzer/classifier.go
+++ analyzer/classifier.go
@@ -1,1 +1,42 @@
 package analyzer
+
+// Packet is the representation of a unit piece of network data.
+type Packet struct {
+	SrcIp      string  // Source IP Address
+	DestIp     string  // Destination IP Address
+	SrcPort    uint16  // Source Port Address
+	DestPort   uint16  // Destination Port Address
+	Protocol   string  // Packet Protocol
+	Payload    []byte  // Raw Bytes in Packet.
+	Size       int     // Total Packet Size (For Stats and Heuristics)
+	PacketRate float64 // Packets per second on this connection (Heuristics)
+	Flags      TCPFlags
+}
+
+// TCP / Control Flags of a packet.
+type TCPFlags struct {
+	SYN bool // Synchronize
+	ACK bool // Acknowledgement
+	FIN bool // Finish
+	RST bool // Reset
+	PSH bool // Push
+	URG bool // Urgent
+}
+
+type Classifier interface {
+	Classify(p *Packet) bool
+}
+
+type RuleClassifier struct {
+}
+
+type ANNClassifier struct {
+}
+
+func (r *RuleClassifier) Classify(p *Packet) bool {
+	return p.Flags.SYN && !p.Flags.ACK
+}
+
+func (a *ANNClassifier) Classify(p *Packet) bool {
+	return false
+}
