e389e0daf0f0399ac031bc3c97007f4febb101de
km2795 <kunalkm2795@gmail.com>
2026-03-06 00:40:02 +0530

    main: wire sniffer and analyzer into working pipeline

--- main.go
+++ main.go
@@ -1,4 +1,52 @@
 package main
 
+import (
+	"fmt"
+	"sr79-fw/analyzer"
+	"sr79-fw/sniffer"
+)
+
 func main() {
+	// ---- HANDLING USER INPUT ---- //
+
+	fmt.Println("Please Enter Network Interface Configuration: (eth0, wlp2s0, ...)")
+
+	// Device Interface Configuration.
+	var deviceInterface string
+
+	// Accept user input for the dev interface configuration.
+	fmt.Scan(&deviceInterface)
+
+	if len(deviceInterface) < 1 {
+		fmt.Println("No Device Interface Configuration Entered. Exiting...")
+		return
+	}
+
+	// ---- INITIATE SNIFFER. ---- //
+	packetSource, err := sniffer.Start(deviceInterface)
+	if err != nil {
+		fmt.Printf("Error Initiating Sniffer: %v. Exiting...", err)
+		return
+	}
+
+	// ---- LOAD CLASSIFIER (RULE BASED CLASSIFIER) ---- //
+	c := analyzer.RuleClassifier{}
+
+	// ---- PACKET RECEIVING CHANNEL LOOP ---- //
+	packetChannel := sniffer.ProcessPackets(packetSource)
+
+	// Loop over the gopacket.Packet channel and invoke Analyze() on the packet.
+	for packet := range packetChannel {
+		verdict := analyzer.Analyze(&c, packet)
+
+		// Drop
+		if verdict == analyzer.Drop {
+			fmt.Println("Packet is Invalid, DROPPING it!")
+
+			// Allow.
+		} else {
+			fmt.Println("Packet is Valid, PASSING it!")
+		}
+	}
+
 }
