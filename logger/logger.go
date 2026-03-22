adec86f6e5dfb07b308b1e07f3e6ffe37352b344
km2795 <kunalkm2795@gmail.com>
2026-03-22 07:47:10 +0530

    logger: "add async log mechanism"

--- logger/logger.go
+++ logger/logger.go
@@ -2,6 +2,9 @@ package logger
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"time"
 )
 
@@ -24,14 +27,44 @@ type LogEntry struct {
 	Verdict     string
 }
 
+var logChan = make(chan LogEntry, 1000)
+var globalLogger *log.Logger
+
+func StartLogger(toFile bool) {
+	var writer io.Writer = os.Stdout
+
+	if toFile {
+		logFile, err := os.OpenFile("classifier.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatalf("Failed to open log file: %v", err)
+		}
+		writer = io.MultiWriter(logFile)
+	}
+
+	globalLogger = log.New(writer, "", 0)
+
+	go func() {
+		for entry := range logChan {
+			globalLogger.Printf("[%s] [%s] [%s] [%s -> %s] FLAGS: %s RATE: %.2f VERDICT: %s\n",
+				entry.Timestamp.Format("02-01-2006 15:04:05"),
+				entry.Level,
+				entry.Classifier,
+				entry.Source,
+				entry.Destination,
+				entry.TCPFlags,
+				entry.PacketRate,
+				entry.Verdict)
+		}
+	}()
+}
+
 func Log(entry LogEntry) {
-	fmt.Printf("[%s] [%s] [%s] [%s -> %s] FLAGS: %s RATE: %.2f VERDICT: %s\n",
-		entry.Timestamp.Format("02-01-2006 15:04:05"),
-		entry.Level,
-		entry.Classifier,
-		entry.Source,
-		entry.Destination,
-		entry.TCPFlags,
-		entry.PacketRate,
-		entry.Verdict)
+	if globalLogger == nil {
+		return
+	}
+	select {
+	case logChan <- entry:
+	default:
+		fmt.Fprintf(os.Stderr, "WARN: log channel full, dropping entry\n")
+	}
 }
